8ba815029c5296413f63f80411f99027f7d62f09
Project 17 <neveraskedfor@gmail.com>
2019-04-02 03:19:50 +0300

    Successfully added ability to get quotes by bypassing CORS.

--- Scripts/test-script.js
+++ Scripts/test-script.js
@@ -1,22 +1,29 @@
 const url = 'http://api.forismatic.com/api/1.0/?';
 const queryMethod = 'method=getQuote';
-const queryType = '&format=json';
+const queryType = '&format=jsonp';
 const queryLang = '&lang=ru';
-const queryUrl = url + queryMethod + queryType + queryLang;
+const queryFunc = '&jsonp=?';
+const queryUrl = url + queryMethod + queryLang + queryType + queryFunc;
+
+let renderQuote = (response) => {
+    let quote = document.createElement("p");
+    quote.innerHTML = response.quoteText;
+    document.body.appendChild(quote);
+}
 
 const xhr = new XMLHttpRequest();
-xhr.responseType = 'json';
-console.log('hi');
-let paragraph1 = document.createElement('p');
-paragraph1.innerHTML = queryUrl;
-document.body.appendChild(paragraph1);
+xhr.responseType = "json";
+const getQuote = document.createElement('script');
+getQuote.src = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=renderQuote';
+document.getElementsByTagName("head")[0].appendChild(getQuote);
 
 xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
         console.log(xhr.response.quoteText);
-        let paragraph = document.createElement('p');
+        let quote = document.createElement("script");
+        quote.src = 'http://api.forismatic.com/api/jsonp/';
+        document.querySelector('head').appendChild(quote);
         paragraph.innerHTML = `<text>${JSON.stringify(xhr.response)}</text>`;
-        document.body.appendChild(paragraph);
     }
 }
 
